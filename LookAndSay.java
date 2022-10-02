86cea867275083c493119067b7101ed20fd10b7f
roach <roach@woowahan.com>
2022-10-02 11:42:24 +0900

    refactoring

--- LookAndSay.java
+++ LookAndSay.java
@@ -1,26 +1,34 @@
 public class LookAndSay {
-    public static void main(String... args) {
+    private static String ant(int n) {
         String s = "1";
-        for (int line = 0; line < 10; line++) {
-            int length = 1;
-            char head = s.charAt(0);
-            String result = "";
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == head) {
-                    length++;
-                } else {
-                    result += length;
-                    result += head;
-                    length = 1;
-                    head = s.charAt(i);
-                }
-            }
-
-            result += length;
-            result += head;
-            s = result;
+        for (int line = 0; line < n; line++) {
+            s = next(s);
         }
-        System.out.println(s);
+        return s;
+    }
+
+    private static String next(String s) {
+        int length = 1;
+        char head = s.charAt(0);
+        String result = "";
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == head) {
+                length++;
+            } else {
+                result += length;
+                result += head;
+                length = 1; // reset
+                head = s.charAt(i);
+            }
+        }
+        result += length;
+        result += head;
+        return result;
+    }
+
+    public static void main(String... args) {
+        var result = ant(10); 
+        System.out.println(result);
     }
 }
